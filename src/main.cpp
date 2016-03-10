a401e8a6d7e153c63f2c812d2396f9cec0e9d4ac
darkfire613 <darkfire613@me.com>
2016-03-10 17:07:04 -0600

    locked framerate to 60

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,28 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT  = 480;
 
+const int SCREEN_FPS = 60;
+const int SCREEN_TICKS_PER_FRAME = 1000 / SCREEN_FPS;
+
 int main (int argc, char* argv[])
 {
   Game* theGame = new Game();
+
   if(theGame->setup("Open Space", 100,100,SCREEN_WIDTH,SCREEN_HEIGHT,0))
   {
     while (theGame->isRunning())
     {
+      int startTime = SDL_GetTicks();
       theGame->handleInput();
       theGame->update();
       theGame->draw();
+
+      // wait for framerate
+      int frameTicks = SDL_GetTicks() - startTime;
+      if (frameTicks < SCREEN_TICKS_PER_FRAME)
+      {
+        SDL_Delay(SCREEN_TICKS_PER_FRAME - frameTicks);
+      }
     }
   }
   else
